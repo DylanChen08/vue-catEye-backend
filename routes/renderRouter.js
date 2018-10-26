11a208a8dd147ff3bee73a9cca52cea4c6f1c853
ease <easetreychen@gmail.com>
2018-10-26 16:02:38 +0800

    调整目录结构

--- routes/renderRouter.js
+++ routes/renderRouter.js
@@ -3,20 +3,11 @@ import Index from "../controller/common/indexList";
 import Release from "../controller/common/releaseList";
 
 
-
 const router = express.Router();
-
-
-
 
 
 router.get("/index", Index.indexMovieRender);    //首页
 router.get("/released", Release.releaseMovieRender);  //正在上映的电影
-
-
-
-
-
 
 
 export default router;
