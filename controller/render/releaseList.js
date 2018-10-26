43088dbed63d1d87ed315bb47c657a93a81ff30c
ease <easetreychen@gmail.com>
2018-10-26 13:02:10 +0800

    新增 电影-正在上映 的数据提供接口

--- controller/render/releaseList.js
+++ controller/render/releaseList.js
@@ -12,7 +12,6 @@ class ReleaseList {
         console.log(results)
         res.send(results)
     }
-
 }
 
 
--- routes/releaseList.js
+++ routes/releaseList.js
@@ -1,8 +1,8 @@
 console.log(41191)
 
 import express from "express";
-import Release from "../controller/render/releaseList";
+import Release from "../controller/render/indexList";
 const router = express.Router();
-router.get("/", Release.releaseMovieRender);
+router.get("/", Release.indexMovieRender);
 
 export default router;
